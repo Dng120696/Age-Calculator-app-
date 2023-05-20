6afeb752b1d1888a273eb5d29ba54763c2c4134c
Dng16 <dongyang0016@gmail.com>
2023-05-20 22:03:23 +0800

    refactor js code

--- script.js
+++ script.js
@@ -13,88 +13,56 @@ const totalMonth = document.querySelector('.total-months');
 const totalYear = document.querySelector('.total-years');
 
 
+const errorMessage = (inputs,val1,val2) =>{
+  if(inputs.value < val1 || inputs.value > val2) {
 
-
+    inputs.parentElement.classList.add('invalid-error');
+    if(inputs.parentElement.contains('invalid-error')){
+      return 0;
+    }
+    
+  }else{
+    inputs.parentElement.classList.remove('invalid-error');
+  }
+}
 
 
 function validate() {
 
   let validator = true;
 
-  for(let i = 0; i < input.length;i++) {
+  for(const inputs of input) {
 
-    if (!input[i].value) {
-      input[i].parentElement.classList.add('error');
-          if (input[i].id === 'number1' || input[i].id === 'number2' || input[i].id === 'number3'){
-            if(!input[i].value) {
-              input[i].parentElement.classList.remove('invalid-error');
-             
+    if (!inputs.value) {
+      inputs.parentElement.classList.add('error');
+          if (inputs.id === 'number1' || inputs.id === 'number2' || inputs.id === 'number3'){
+            if(!inputs.value) {
+              inputs.parentElement.classList.remove('invalid-error');    
             } 
-          
-          
-          }
-      
+          }     
           validator = false;
         }
           else {
-       
-                if (input[i].id === 'number1'){
-                  if(input[i].value < 1 || input[i].value > 31) {
-
-                    input[i].parentElement.classList.add('invalid-error');
-                    if(input[i].parentElement.contains('invalid-error')){
-                      return 0;
-                    }
-                    
-                  }else{
-                    input[i].parentElement.classList.remove('invalid-error');
-                  
-                  }
+                if (inputs.id === 'number1'){
+                 errorMessage(inputs,1,31);
                 }
-                if (input[i].id === 'number2'){
-                  if(input[i].value < 1 || input[i].value > 12) {
-             
-                    input[i].parentElement.classList.add('invalid-error');
-            
-                    if(input[i].parentElement.contains('invalid-error')){
-                      return 0;
-                    }
-                  }else{
-                    input[i].parentElement.classList.remove('invalid-error');
-                  
-                  }
-                }
-                
-                if (input[i].id === 'number3'){
-                  if(input[i].value < 1 || input[i].value > getYear) {
-                    input[i].parentElement.classList.add('invalid-error');
-                    if(input[i].parentElement.contains('invalid-error')){
-                      return 0;
-                    }
-                  }else{
-                    input[i].parentElement.classList.remove('invalid-error');
-                  
-                  }
-                }
-                
-              
-                input[i].parentElement.classList.remove('error');
-                  validator = true;
-                
-               
-              }
-             
-            }
-       
+                if (inputs.id === 'number2'){
+                  errorMessage(inputs,1,12)
+                }         
+                if (inputs.id === 'number3'){
+                  errorMessage(inputs,1,getYear)
+                }  
+                inputs.parentElement.classList.remove('error');
+                  validator = true;             
+              }           
+            }    
   return validator;
 };
-
 
 const date = new Date();
 let getYear = date.getFullYear();//2023
 let getMonth = date.getMonth() + 1;//4
 let getDay = date.getDate();//9
-
 
 const months = [31,28,30,31,30,31,30,31,30,31,30,31];
 
@@ -104,14 +72,10 @@ const handleSumbit = (e) => {
     if (inputDay.value > getDay) {//12 > 9 = true
       getDay = getDay + months[getMonth - 1]; // 9 + 31 = 40
       getMonth = getMonth - 1;//3
-
     }
     if (inputMonth.value > getMonth) {// 12 > 3 = true
       getMonth = getMonth + 12; //3+12 = 15
-      getYear = getYear - 1;//2023 - 1 = 2022
-
-     
-    
+      getYear = getYear - 1;//2023 - 1 = 2022   
     }
 
     const day = getDay - inputDay.value;//40 - 12 = 28
@@ -129,11 +93,11 @@ form.addEventListener("submit", handleSumbit);
 
 const arr =[inputDay,inputMonth,inputYear];
 
-for(let k = 0; k < arr.length;k++){
- arr[k].addEventListener('focus',() =>{
+for(const arrs of arr){
+ arrs.addEventListener('focus',() =>{
   
-    arr[k].parentElement.classList.remove('error');
-    arr[k].parentElement.classList.remove('valid-error');
+    arrs.parentElement.classList.remove('error');
+    arrs.parentElement.classList.remove('valid-error');
     
   });
 }
